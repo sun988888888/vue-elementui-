5466b8c511b6a3e9cb68b7f2d85f08c0573f6c42
wangxiangbo <2464134305@qq.com>
2023-01-12 10:36:21 +0800

    新增身份拦截

--- src/utils/serves.js
+++ src/utils/serves.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import { getStorage } from '@/utils/setStorage'
 import { Message } from 'element-ui'
-
+import $store from '@/store'
+import router from "@/router";
 const service = axios.create({
   baseURL: process.env.VUE_APP_API_BASE_URL,//baseurl会自动加在请求地址
   timeout: 20000
@@ -9,6 +10,10 @@ const service = axios.create({
 
 /* 请求拦截 */
 service.interceptors.request.use((config) => {
+  /* 没有身份的话重新登陆 */
+  if(config.data.api!='user@login' && $store.state.user.identity===''){
+    router.push('/')
+  }
   //获取并设置token
   config.headers['token'] = getStorage('token')
   return config
@@ -20,7 +25,7 @@ service.interceptors.response.use((response) => {
   //对响应做些什么
   let { code, msg } = response.data
   if (code == 501) {
-    this.$router.push('/')
+    router.push('/')
     return Message({ message: msg || 'error', type: "warning" })
   }
   return Promise.resolve(response.data)
